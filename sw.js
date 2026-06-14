5153e451878bf64b74949f56bcec9ff03bee9017
tad551216-rgb <tad551216@gmail.com>
2026-06-15 07:00:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kakeibo-v22";
+const CACHE = "kakeibo-v23";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
